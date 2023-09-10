bb226a95d7015a993061fa7a382cadd63b170c6b
Ridwan-Lawal <lawal6382@gmail.com>
2023-09-10 12:06:37 +0100

    built the success page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
         grey: "hsl(231, 7%, 60%)",
         white: "hsl(0, 0%, 100%)",
       },
+
+      backgroundImage: {
+        mobile: "url('/assets/images/illustration-sign-up-mobile.svg')",
+        desktop: "url('/assets/images/illustration-sign-up-desktop.svg')",
+      },
     },
   },
   plugins: [],
